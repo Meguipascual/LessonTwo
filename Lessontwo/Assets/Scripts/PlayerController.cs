f78b0328e80c72bdf5da0b3a9ffee9e5006a112d
meguipascual <meguipascual@gmail.com>
2021-09-15 02:15:27 +0200

    Now the player can move sideways without restriction which is bad

--- Lessontwo/Assets/Scripts/PlayerController.cs
+++ Lessontwo/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
         
     }
 }
